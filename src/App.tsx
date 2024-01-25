5834b8e4506406b98597db847f6a33b43d111206
shree3245 <shreevenkr@gmail.com>
2024-01-24 18:52:40 -0800

    created UI to display code, CSS is not good, need to update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MonacoEditor from "react-monaco-editor";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [content, setContent] = useState("");
+
+	useEffect(() => {
+		const fetchDocument = async () => {
+			const response = await fetch(
+				"https://revival-file-upload-bucket.s3.amazonaws.com/b724cc82-ba66-11ee-b269-faa9c0ada648.py?AWSAccessKeyId=AKIAQIMM7EL4N7YSO5SM&Signature=uszM43NgyBoLinaptvpBXsySY4E%3D&Expires=1706154214"
+			);
+			const text = await response.text();
+			console.log(text);
+			setContent(text);
+		};
+
+		fetchDocument();
+	}, []);
+
+	return (
+		<div className="App">
+			<MonacoEditor
+				// width="800"
+				// height="600"
+				language="javascript"
+				theme="vs-dark"
+				value={content}
+				options={{
+					automaticLayout: true,
+				}}
+			/>
+		</div>
+	);
 }
 
 export default App;
